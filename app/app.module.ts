df8a60fd4ade8504cba91ca060391e8a71ec3fd0
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-08-31 16:53:03 +0530

    Code

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import { Child3Component } from './child3.component';
+import { Child4Component } from './child2/child4/child4.component';
 
 @NgModule({
-  declarations:[AppComponent],
+  declarations:[AppComponent, Child1Component, Child2Component, Child3Component, Child4Component],
   imports:[BrowserModule],
   bootstrap:[AppComponent]
 })
