b7e9faba256bdfdca77f088d81257eef9733e85d
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-09-03 22:58:01 +0530

    Code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,54 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-myroot',
- // template:`<h1>I am App Component</h1>`
- templateUrl:'./app.component.html',
- styleUrls:['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-  recFromChild:string;
-  data:string;
-  title:string;
-  myclass:string;
-  flag:boolean ;
-  counter:number;
-  amount:number;
-  finalnum:string='0';
-  buttons:number [] = [1,2,3,4,5,6,7,8,9];
-  takeNumber(num){
-    if(this.finalnum=='0'){
-      
-      this.finalnum = num.toString();
-    }
-    else{
-    this.finalnum +=num;
-    }
-  }
-  constructor(){
-    this.amount = 0;
-    this.counter = 0;
-    this.flag = true;
-    this.myclass = 'red';
-    this.title="Hello Angular - 2019";
-  }
-  callParentByChild(evt){
-    this.recFromChild = evt;
-  }
-  fillData(evt){
-    this.data = evt.target.value;
-  }
-  takeAmount(evt){
-    this.amount = evt.target.value;
-  }
-  takeInput(evt){
-    this.title = evt.target.value;
-  }
-  count(){
-    this.counter++;
-  }
-  update(){
-    this.flag = !this.flag;
-  }
-
+export class AppComponent {
+  title = 'shopapp';
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import {BrowserModule} from '@angular/platform-browser';
-import { Child1Component } from './child1/child1.component';
-import { Child2Component } from './child2/child2.component';
-import { Child3Component } from './child3.component';
-import { Child4Component } from './child2/child4/child4.component';
+import { ShopComponent } from './shop/shop.component';
+import { SearchComponent } from './shop/search/search.component';
+import { SortComponent } from './shop/sort/sort.component';
+import { ListComponent } from './shop/list/list.component';
+import { ItemComponent } from './shop/list/item/item.component';
+import { ItemcountComponent } from './shop/itemcount/itemcount.component';
 
 @NgModule({
-  declarations:[AppComponent, Child1Component, Child2Component, Child3Component, Child4Component],
-  imports:[BrowserModule],
-  bootstrap:[AppComponent]
+  declarations: [
+    AppComponent,
+    ShopComponent,
+    SearchComponent,
+    SortComponent,
+    ListComponent,
+    ItemComponent,
+    ItemcountComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule{
-
-}
+export class AppModule { }
